2f70f0459411a1254bfc46117b41251e0e558cb4
TribucioNeil <144679083+TribucioNeil@users.noreply.github.com>
2023-09-23 23:23:23 +0800

    final

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -5,7 +5,46 @@ namespace App\Controllers;
 use App\Controllers\BaseController;
 
 class ProductController extends BaseController
-{
+{    
+    private $product;
+    public function __construct()
+    {
+        $this->product = new \App\Models\ProductModel();
+    }
+    public function edit($ProdId)
+    {
+        $data =[
+            'product' => $this->product->findAll(),
+            'pro' => $this->product->where('ProdId', $ProdId)->first(),
+        ];
+            return view('products', $data);
+    }
+    public function save()
+    {
+       $ProdId =$_POST['ProdId'];
+       $data = [
+        'ProductName'=> $this->request->getVar('ProductName'),          
+        'ProductDescription'=> $this->request->getVar('ProductDescription'),
+        'ProductCategory' => $this->request->getVar('ProductCategory'),
+        'ProductQuantity'=> $this->request->getVar('ProductQuantity'),
+        'ProductPrice'=> $this->request->getVar('ProductPrice'),
+        ];
+        if($ProdId!= null){
+         $this->product->set($data)->where('ProdId', $ProdId)->update();
+       }else{
+       $this->product->save($data);
+      }
+      return redirect()->to('/product');
+    }
+
+
+    public function delete($ProdId)
+    {
+        $this->product->delete($ProdId);
+        return redirect()->to('/product');
+    }
+    
+   
     public function product($product)
     {
         echo $product;
@@ -16,7 +55,7 @@ class ProductController extends BaseController
     }
     public function neil()
     {
-        return view ('products');
-
+        $data['product'] = $this->product->FindAll();
+        return view ('products',$data);
     }
 }
--- app/Views/products.php
+++ app/Views/products.php
@@ -3,9 +3,94 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Products</title>
+    <style>form {
+  width: 100%;
+  max-width: 500px;
+  margin: 0 auto;
+}
+
+.form-group {
+  margin-bottom: 10px;
+}
+
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+input,
+select {
+  width: 100%;
+  padding: 5px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+input[type="submit"] {
+  background-color: #000;
+  color: #fff;
+  border: none;
+  padding: 10px;
+  cursor: pointer;
+}
+</style>
+
 </head>
 <body>
-    <h1> yess naman </h1>
+    
+    <form action ="/save" method ="post">  
+    <div class="form-group">
+    <label for="ProductName">Product Name</label>
+      <input type="hidden" name="ProdId" value="<?= isset($pro) ? $pro['ProdId'] : '' ?>">
+      <input type ="text" name ="ProductName" placeholder="Enter the product name" value="<?= isset($pro) ? $pro['ProductName'] : '' ?>">
+      </div>
+      <div class="form-group">
+      <label for="ProductDescription">Product Description</label>
+      <input type ="text" name ="ProductDescription" placeholder="Enter a brief description of the product" value="<?= isset($pro) ? $pro['ProductDescription'] : '' ?>">
+      </div>
+      <div class="form-group">
+     <label for="ProductCategory">Product Category</label>
+     <select class="form-control" id="ProductCategory" name="ProductCategory">
+      <option value="Snacks and desserts">Snacks and desserts</option>
+      <option value="Bakery products">Bakery products</option>
+      <option value="Dairy products">Dairy products</option>
+      <option value="Beverages">Beverages</option>
+      <option value="Grains and cereals">Grains and cereals</option>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="ProductQuantity">Product Quantity</label>
+      <input type ="number" name ="ProductQuantity" placeholder="Enter the quantity of the product" value="<?= isset($pro) ? $pro['ProductQuantity'] : '' ?>">
+      </div>
+      <div class="form-group">
+    <label for="ProductPrice">Product Price</label>
+    <input type="number" class="form-control" name ="ProductPrice" placeholder="Enter the price of the product" value="<?= isset($pro) ? $pro['ProductPrice'] : '' ?>">
+      <br>
+      <input type ="submit" value="save">
+    </form>
+
+    <h2>Product Listing</h2>
+    <table border="1">
+        <tr>
+            <th>ProductName</th>
+            <th>ProductDescription:</th>
+            <th>ProductCategory</th>
+            <th>ProductQuantity</th>
+            <th>ProductPrice</th>
+            <th>Action</th>
+       </tr>
+       <?php foreach ($product as $pr): ?>
+            <tr>
+                <td><?= $pr['ProductName'] ?></td>
+                <td><?= $pr['ProductDescription'] ?></td>
+                <td> <?= $pr['ProductCategory'] ?></td>
+                <td> <?= $pr['ProductQuantity'] ?></td>
+                <td> <?= $pr['ProductPrice'] ?></td>
+                <td> <a href="/delete/<?= $pr['ProdId'] ?>">delete</a>|| <a href="/edit/<?= $pr['ProdId'] ?>">edit</a> </td>
+                
+            </tr>
+        <?php endforeach; ?>
+    </table>   
 </body>
 </html>
